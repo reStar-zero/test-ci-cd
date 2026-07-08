ca221a002cfe990a0d4c1f24086fb40b6776381c
reStar-zero <crygstar@gmail.com>
2026-07-08 17:22:52 +0300

    Первый комиит: добавлены тесты и CI/CD

---
+++ app.py
@@ -0,0 +1,18 @@
+python
+def add(a, b):
+    """Простая функция сложения"""
+    return a + b
+
+def multiply(a, b):
+    """Простая функция умножения"""
+    return a * b
+
+def divide(a, b):
+    """Функция деления"""
+    if b == 0:
+        raise ValueError("Нельзя делить на ноль!")
+    return a / b
+
+if __name__ == "__main__":
+    print(f"2 + 3 = {add(2, 3)}")
+    print(f"2 * 3 = {multiply(2, 3)}")
