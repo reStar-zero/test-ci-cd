de63d85d68b016a02e7ab20a1f25e3ec95bb803e
reStar-zero <crygstar@gmail.com>
2026-07-08 17:37:03 +0300

    Fix: исправлены стилистические ошибки в коде

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-python
 def add(a, b):
     """Простая функция сложения"""
     return a + b
 
+
 def multiply(a, b):
     """Простая функция умножения"""
     return a * b
+
 
 def divide(a, b):
     """Функция деления"""
@@ -13,6 +14,8 @@ def divide(a, b):
         raise ValueError("Нельзя делить на ноль!")
     return a / b
 
+
 if __name__ == "__main__":
     print(f"2 + 3 = {add(2, 3)}")
     print(f"2 * 3 = {multiply(2, 3)}")
+    print(f"10 / 2 = {divide(10, 2)}")
